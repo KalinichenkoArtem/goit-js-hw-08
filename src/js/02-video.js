05beb64e2a3f879aa11568a44686dc30175accf7
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-01-08 14:26:14 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,30 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
-console.log(Player);
+const iframe = document.querySelector('iframe');
+
+const KEY_LOCAL = "videoplayer-current-time";
+
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+    const timeOff = JSON.stringify(data);
+    localStorage.setItem(KEY_LOCAL, timeOff);
+});
+
+const timeOn = JSON.parse(localStorage.getItem(KEY_LOCAL));
+let timeMove = timeOn;
+
+player.setCurrentTime(timeMove.seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
