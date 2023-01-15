93d977830ea848099f022aee78fdef23893ba716
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-01-15 10:41:46 +0200

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,13 @@
 var throttle = require('lodash.throttle');
 
 console.log(throttle);
+
+const refs = {
+    form: document.querySelector("form"),
+    textarea: document.querySelector("textarea"),
+};
+
+console.log(refs);
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.textarea.addEventListener("input", onTextareaInput);
