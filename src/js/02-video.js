b6770d560d8e369015068ec6c21c523205c9519d
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-02-25 23:35:09 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,14 +10,13 @@ const player = new Player(iframe);
 
 player.on('timeupdate', function (data) {
     const timeOff = JSON.stringify(data);
-    localStorage.setItem(KEY_LOCAL, timeOff);
+   localStorage.setItem(KEY_LOCAL, timeOff);
 });
 
 const timeOn = JSON.parse(localStorage.getItem(KEY_LOCAL));
 
-
 player
-    .setCurrentTime(timeOn)
+    .setCurrentTime(timeOn.seconds)
     .then(function (seconds) {})
     .catch(function (error) {
     switch (error.name) {
